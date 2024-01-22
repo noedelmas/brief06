f209660494468072cfd134950d4e5ec181008b27
noedmw <noe.dwwm48@gmail.com>
2024-01-22 16:17:17 +0100

    16h16 22/01/2024
    barres bleues pour les titres

--- src/index.php
+++ src/index.php
@@ -7,25 +7,25 @@ $favoris = $result->fetchAll(PDO::FETCH_ASSOC);
 
     <section id="bookmarks">
         <table>
-            <tr class="rounded-md border border-orange-500">
-                <th>Domaine</th>
-                <th>Libellé</th>
-                <th>Date ajout</th>
-                <th>Lien</th>
-                <th>Update</th>
-                <th>Delete</th>
+            <tr class=" border border-orange-500">
+                <th class="bg-sky-500">Domaine</th>
+                <th class="bg-sky-500">Libellé</th>
+                <th class="bg-sky-500">Date ajout</th>
+                <th class="bg-sky-500">Lien</th>
+                <th class="bg-sky-500">Update</th>
+                <th class="bg-sky-500">Delete</th>
             </tr>
             <?php
                 foreach($favoris as $favori){
             ?>
 
             <tr>
-                <td><?php echo $favori['nom_dom']?></td>
-                <td><?php echo $favori['libelle']?></td>
-                <td><?php echo $favori['date_creation']?></td>
-                <td><?php echo $favori['url']?></td>
-                <td><i class="fa-solid fa-rotate"></i></td>
-                <td><i class="fa-solid fa-trash-can"></i></td>
+                <td class="rounded-md border border-orange-500"><?php echo $favori['nom_dom']?></td>
+                <td class="rounded-md border border-orange-500"><?php echo $favori['libelle']?></td>
+                <td class="rounded-md border border-orange-500"><?php echo $favori['date_creation']?></td>
+                <td class="rounded-md border border-orange-500"><?php echo $favori['url']?></td>
+                <td class="rounded-md border border-orange-500"><i class="fa-solid fa-rotate"></i></td>
+                <td class="rounded-md border border-orange-500"><i class="fa-solid fa-trash-can"></i></td>
             </tr>
 
             <?php
