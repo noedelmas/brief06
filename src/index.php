d47c672deaf6839536dc0d882c7bff31f69c2797
noedmw <noe.dwwm48@gmail.com>
2024-01-23 16:24:57 +0100

    commentaires
    écriture d'une description du code.

--- src/index.php
+++ src/index.php
@@ -1,26 +1,46 @@
+    <!-- Inclusion du header et du pdo via la fonction include.php afin d'afficher le <h1> "Mes favoris". -->
+
 <?php
 include("header.php");
 include("pdo.php");
 
 ?>
+
+        <!-- Création de la section 'filtre_domaine' afin  de pouvoir les filtrer par la suite. -->
+
     <section class="filtre_domaine flex justify-center">
         <form action="" method="GET">
+
+                <!-- Déclaration du nom : 'filtre_dom' puis création du résultat selon le domaine sur lequel on clique. -->
+
             <select name="filtre_dom" class="my-8 rounded rounded-md hover:bg-slate-500">
             <?php
             $result = $pdo->query("SELECT * FROM domaine");
             $domaine = $result->fetchAll(PDO::FETCH_ASSOC); 
                 foreach($domaine as $un_domaine){
             ?>
+
+                    <!-- Création/décla d'une option afin de faire comprendre à la machine qu'il faut afficher le domaine par rapport à l'id. -->
+
                 <option value="<?php echo $un_domaine['id_dom']?>"><?php echo $un_domaine['nom_dom']?></option>
                 <?php
                 }
             ?>
             </select>
+
+                <!-- Création/décla d'un bouton afin de soumettre son résultat pour ensuite éxécuter l'affichage. -->
+
             <button class="my-4 text align-center rounded rouned-md hover:bg-slate-500" type="submit">
                 Appliquer
             </button>
+
+                <!-- Fermeture du bloc formulaire. -->
+
         </form>
     </section>
+
+                <!-- Déclaration de la condition, si le 'filtre_dom' est défini alors la requête sql sélectionne l'id correspondant, l'affiche dans l'url et sur la page en cours. -->
+
             <?php 
                 if (isset($_GET['filtre_dom'])){
                     print_r($_GET['filtre_dom']);
@@ -31,9 +51,18 @@ include("pdo.php");
                 $result = $pdo->query($rqtsql);
                 $favoris = $result->fetchAll(PDO::FETCH_ASSOC); 
             ?>
+
+                <!-- Création de la classe section. -->
+
     <section class="flex items-center justify-center">
+
+                <!-- Création de la table afin d'afficher les en-têtes + contour orange. -->
+
         <table>
             <tr class="border border-amber-500">
+
+                    <!-- Création de chaque en-tête 1 par 1, colonne par colonne. -->
+            
                 <th class="bg-sky-500">Domaine</th>
                 <th class="bg-sky-500">Libellé</th>
                 <th class="bg-sky-500">Date ajout</th>
@@ -41,17 +70,25 @@ include("pdo.php");
                 <th class="bg-sky-500">Update</th>
                 <th class="bg-sky-500">Delete</th>
             </tr>
+
+                <!-- Appel de variable pour afficher les favoris dynamiquement. -->
+
             <?php
                 foreach($favoris as $favori){
             ?>
 
-            <tr class="old:bg-white even:bg-orange-200 hover:bg-zinc-500">
-                <td class="border border-orange-500 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white dark:hover:bg-gray-600"><?php echo $favori['nom_dom']?></td>
-                <td class="border border-orange-500 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white dark:hover:bg-gray-600"><?php echo $favori['libelle']?></td>
-                <td class="border border-orange-500 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white dark:hover:bg-gray-600"><?php echo $favori['date_creation']?></td>
-                <td class="border border-orange-500 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white dark:hover:bg-gray-600"><?php echo $favori['url']?></td>
-                <td class="border border-orange-500 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white dark:hover:bg-gray-600"><button class="fa-solid fa-rotate"></button></td>
-                <td class="border border-orange-500 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white dark:hover:bg-gray-600"><button class="fa-solid fa-trash-can"></button></td>
+                <!-- Création de l'affichage des différents colonnes (1 sur 2 s'affiche en couleur.) -->
+
+            <tr class="old:bg-white even:bg-amber-200 hover:bg-zinc-500">
+
+                <!-- Création du contour orange + l'animation quand on passe la souris dessus. -->
+
+                <td class="border border-amber-500 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white dark:hover:bg-gray-600"><?php echo $favori['nom_dom']?></td>
+                <td class="border border-amber-500 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white dark:hover:bg-gray-600"><?php echo $favori['libelle']?></td>
+                <td class="border border-amber-500 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white dark:hover:bg-gray-600"><?php echo $favori['date_creation']?></td>
+                <td class="border border-amber-500 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white dark:hover:bg-gray-600"><?php echo $favori['url']?></td>
+                <td class="border border-amber-500 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white dark:hover:bg-gray-600"><button class="fa-solid fa-rotate" type="submit"></button></td>
+                <td class="border border-amber-500 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white dark:hover:bg-gray-600"><button class="fa-solid fa-trash-can" type="submit"></button></td>
             </tr>
                 
             <?php
